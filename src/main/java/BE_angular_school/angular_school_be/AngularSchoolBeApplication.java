3eff3f867321205518d5d7fc6668ab806fb4aab1
Mgalazyn <m.galazynn@gmail.com>
2024-08-14 11:42:35 +0200

    initial commit

---
+++ src/main/java/BE_angular_school/angular_school_be/AngularSchoolBeApplication.java
@@ -0,0 +1,13 @@
+package BE_angular_school.angular_school_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSchoolBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSchoolBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BE_angular_school/angular_school_be/AngularSchoolBeApplicationTests.java
@@ -0,0 +1,13 @@
+package BE_angular_school.angular_school_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSchoolBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
